8311c1320441e26e500b690a92c3e384576e86cf
Homere Faivre <homere.faivre@insa-lyon.fr>
2014-03-31 23:52:57 +0800

    ack

--- apps/AutoConfig/AutoConfig.h
+++ apps/AutoConfig/AutoConfig.h
@@ -4,13 +4,16 @@
  
 enum {
 	AM_AUTOCONFIGMSG = 1,
-	TIMEOUT_PERIOD_MILLI = 2000
+	TIMEOUT_PERIOD_MILLI = 2000,
+	IS_AUTOCONFIGACK = 1,
+	NOT_AUTOCONFIGACK = 0
 };
  
 typedef nx_struct AutoConfigMsg {
  	nx_uint8_t srcRank;
  	nx_uint8_t dstRank;
  	nx_uint8_t txPower;
+ 	nx_uint8_t ack;
 } AutoConfigMsg;
 
 #endif
